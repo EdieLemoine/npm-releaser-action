5b5426e803370b2d38f0e5ea7aa08a98b71af2a4
Edie Lemoine <edie@myparcel.nl>
2020-04-07 16:44:07 +0200

    commit

---
+++ src/main.js
@@ -0,0 +1,20 @@
+const core = require('@actions/core');
+const github = require('@actions/github');
+const standardVersion = require('standard-version');
+
+async function run() {
+
+  try {
+    const dryRun = core.getInput('dry-run') || true;
+
+    const output = standardVersion({ dryRun });
+
+    console.log(output);
+    const payload = JSON.stringify(github.context.payload, undefined, 2);
+    console.log(`The event payload: ${payload}`);
+  } catch (error) {
+    core.setFailed(error.message);
+  }
+}
+
+run();
