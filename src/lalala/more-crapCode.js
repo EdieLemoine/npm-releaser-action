b122ba954cf4002780d293883ce54b5e16437733
Edie Lemoine <edie@myparcel.nl>
2021-07-27 13:55:12 +0200

    fix: update 2021-07-27 13:55:12

---
+++ src/lalala/more-crapCode.js
@@ -0,0 +1,13 @@
+var bla;
+
+if(typeof(sortOrder) != "boolean"){
+    return items;
+}
+filtered.sort(function (a, b) {
+    if(sortOrder == true){
+        return (CustomOrder(a.status) > CustomOrder(b.status) ? 1 : -1);
+    }
+    else if(sortOrder == false){
+        return (CustomOrder(a.status) < CustomOrder(b.status) ? 1 : -1);
+    }
+});
