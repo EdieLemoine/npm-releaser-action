79019f77548ab05a8d4cd9f371782612768ba56b
Edie Lemoine <edie@myparcel.nl>
2022-02-09 17:15:35 +0100

    wip

---
+++ private/generateCommits.js
@@ -0,0 +1,47 @@
+const randomWords = require('random-words');
+const child_process = require('child_process');
+const path = require('path');
+const fs = require('fs');
+
+const types = ['feat', 'fix', 'refactor', 'style', 'build', 'perf'];
+const scopes = ['order', 'shipment', 'return', 'collect'];
+
+const commits = [];
+
+const [, , amount] = process.argv;
+
+for (let i = 0; i < Number(amount || 1); i++) {
+  const type = types[types.length * Math.random() | 0];
+  let scopeString = '';
+  let scope = null;
+
+  if (Math.random() > 0.5) {
+    scope = scopes[scopes.length * Math.random() | 0];
+    scopeString = `(${scope})`;
+  }
+
+  commits.push({
+    type,
+    scope,
+    message:
+      `${type}${scopeString}: ${randomWords({ min: 4, max: 8, maxLength: 8, join: ' ' })}`,
+  });
+}
+
+// child_process.spawnSync('git', ['checkout', 'develop'], {stdio: 'inherit'});
+commits.forEach((commit) => {
+  const basePath = path.resolve(__dirname, '..', 'src');
+
+  let dir = path.resolve(basePath, 'commits', commit.type);
+  let file = path.resolve(dir, `${commit.scope ?? 'none'}.txt`);
+  child_process.spawnSync('mkdir', ['-p', dir], { stdio: 'inherit' });
+  child_process.spawnSync('touch', [file], { stdio: 'inherit' });
+
+  fs.appendFileSync(file, commit.message + '\n');
+
+  child_process.spawnSync('git', ['add', file], { stdio: 'inherit' });
+  child_process.spawnSync('git', ['commit', '-m', commit.message], { stdio: 'inherit' });
+});
+// child_process.spawnSync('git', ['checkout', '-'], {stdio: 'inherit'});
+
+// console.log(commits);
--- release.config.js
+++ release.config.js
@@ -1,16 +1,63 @@
-const baseConfig = require('@myparcel/semantic-release-config/github');
+const baseConfig = require('@myparcel/semantic-release-config');
+const { addReleaseNotesGeneratorPlugin } = require('@myparcel/semantic-release-config/src/plugins');
 
 module.exports = {
-  extends: '@myparcel/semantic-release-config/github',
+  extends: '@myparcel/semantic-release-config',
   ...baseConfig,
   plugins: [
-    ...baseConfig.plugins,
-    [
-      '@saithodev/semantic-release-backmerge', {
-        branches: [{ from: 'main', to: 'develop' }],
-        message: 'chore(release): rebase published changes [skip ci]',
-        clearWorkspace: true,
-      },
-    ],
+    ['@edielemoine/semantic-release-svn', { url: 'https://plugins.svn.wordpress.org/woocommerce-myparcel' }],
+
+    // addReleaseNotesGeneratorPlugin({
+    //   presetConfig: {
+    //     header: 'Changes',
+    //     commitsSort: ['scope', 'subject'],
+    //     types: [
+    //       {
+    //         type: 'feat',
+    //         section: ':sparkles: New Features',
+    //       },
+    //       {
+    //         type: 'fix',
+    //         section: ':bug: Bug Fixes',
+    //       },
+    //       {
+    //         type: 'perf',
+    //         section: ':zap: Performance Improvements',
+    //       },
+    //       {
+    //         type: 'refactor',
+    //         section: ':broom: Refactors',
+    //       },
+    //       {
+    //         type: 'docs',
+    //         section: ':books: Documentation',
+    //       },
+    //       {
+    //         type: 'revert',
+    //         section: ':rewind: Reverts',
+    //       },
+    //       {
+    //         type: 'chore',
+    //         hidden: true,
+    //       },
+    //       {
+    //         type: 'style',
+    //         hidden: true,
+    //       },
+    //       {
+    //         type: 'test',
+    //         hidden: true,
+    //       },
+    //     ],
+    //   },
+    // }),
+    // [
+    //   '@saithodev/semantic-release-backmerge', {
+    //   branches: [{ from: 'main', to: 'develop' }],
+    //   message: 'chore(release): rebase published changes [skip ci]',
+    //   mergeMode: 'ours',
+    //   clearWorkspace: true,
+    // },
+    // ],
   ],
 };
