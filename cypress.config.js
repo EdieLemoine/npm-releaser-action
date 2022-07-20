b68612078330a706f6c0b94ea39ee037b2314072
Edie Lemoine <edie@myparcel.nl>
2022-07-20 11:13:40 +0200

    fix: update 2022-07-20 11:13:40

--- cypress.config.js
+++ cypress.config.js
@@ -1,6 +1,7 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+  projectId: '17bi72',
   e2e: {
     baseUrl: 'http://127.0.0.1:8080',
     setupNodeEvents(on, config) {
