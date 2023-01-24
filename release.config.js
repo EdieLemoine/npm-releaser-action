e1e573722b0def57fa09f246cf1843c9b21550ce
Edie Lemoine <edie.lemoine@gmail.com>
2023-01-24 14:18:06 +0100

    test command formats

--- release.config.js
+++ release.config.js
@@ -1,6 +1,36 @@
 const baseConfig = require('@myparcel/semantic-release-config');
 const { addGitPlugin, addNpmPlugin, addComposerPlugin } = require('@myparcel/semantic-release-config/src/plugins');
 
+const eofCommand = `sh -c <<EOF
+  echo "OUTPUTTING TO GITHUB_OUTPUT"
+  echo "lastVersion=\${lastRelease.version}" >> $GITHUB_OUTPUT
+
+  echo "releaseType=\${nextRelease.type}" >> $GITHUB_OUTPUT
+  echo "nextVersion=\${nextRelease.version}" >> $GITHUB_OUTPUT
+
+  echo "nextRelease2=\${JSON.encode(nextRelease)}" >> $GITHUB_OUTPUT
+  echo "lastRelease2=\${JSON.encode(lastRelease)}" >> $GITHUB_OUTPUT
+
+  echo "nextRelease=\${nextRelease}" >> $GITHUB_OUTPUT
+  echo "lastRelease=\${lastRelease}" >> $GITHUB_OUTPUT
+
+  echo "Wrote to GITHUB_OUTPUT
+EOF`;
+
+const plainCommand = `echo "OUTPUTTING TO GITHUB_OUTPUT"
+  echo "lastVersion=\${lastRelease.version}" >> $GITHUB_OUTPUT
+
+  echo "releaseType=\${nextRelease.type}" >> $GITHUB_OUTPUT
+  echo "nextVersion=\${nextRelease.version}" >> $GITHUB_OUTPUT
+
+  echo "nextRelease2=\${JSON.encode(nextRelease)}" >> $GITHUB_OUTPUT
+  echo "lastRelease2=\${JSON.encode(lastRelease)}" >> $GITHUB_OUTPUT
+
+  echo "nextRelease=\${nextRelease}" >> $GITHUB_OUTPUT
+  echo "lastRelease=\${lastRelease}" >> $GITHUB_OUTPUT
+
+  echo "Wrote to GITHUB_OUTPUT`;
+
 module.exports = {
   extends: '@myparcel/semantic-release-config',
   ...baseConfig,
@@ -9,16 +39,23 @@ module.exports = {
     [
       '@semantic-release/exec',
       {
-        verifyReleaseCmd: [
-          'echo "lastVersion=${lastRelease.version}" >> $GITHUB_OUTPUT',
-          'echo "releaseType=${nextRelease.type}" >> $GITHUB_OUTPUT',
-          'echo "nextVersion=${nextRelease.version}" >> $GITHUB_OUTPUT',
-          'echo "nextRelease2=${JSON.encode(nextRelease)}" >> $GITHUB_OUTPUT',
-          'echo "lastRelease2=${JSON.encode(lastRelease)}" >> $GITHUB_OUTPUT',
-          'echo "nextRelease=${nextRelease}" >> $GITHUB_OUTPUT',
-          'echo "lastRelease=${lastRelease}" >> $GITHUB_OUTPUT',
-          'echo "Wrote to GITHUB_OUTPUT"',
-        ].join('\n'),
+        verifyReleaseCmd: eofCommand,
+      },
+    ],
+    [
+      '@semantic-release/exec',
+      {
+        verifyReleaseCmd: plainCommand
+          // Filters leading and trailing whitespace.
+          .trim()
+          // Splits all lines into an array.
+          .split('\n')
+          // Removes empty lines.
+          .filter(Boolean)
+          // Trims whitespace off each line. Not necessary, but makes the output look nicer.
+          .map((line) => line.trim())
+          // Joins all lines into a single command with && between each line.
+          .join(' && '),
       },
     ],
     addNpmPlugin(),
