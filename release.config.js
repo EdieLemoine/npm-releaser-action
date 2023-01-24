d253baa05065d0ebcdc8a339fb11c9c6b8eb6b45
Edie Lemoine <edie.lemoine@gmail.com>
2023-01-24 14:02:35 +0100

    fix: update 2023-01-24 14:02:35

--- release.config.js
+++ release.config.js
@@ -9,16 +9,16 @@ module.exports = {
     [
       '@semantic-release/exec',
       {
-        verifyReleaseCmd: `
-          echo "lastVersion=\${lastRelease.version}" >> $GITHUB_OUTPUT
-          echo "releaseType=\${nextRelease.type}" >> $GITHUB_OUTPUT
-          echo "nextVersion=\${nextRelease.version}" >> $GITHUB_OUTPUT
-          
-          echo "nextRelease2=\${JSON.encode(nextRelease)}" >> $GITHUB_OUTPUT
-          echo "lastRelease2=\${JSON.encode(lastRelease)}" >> $GITHUB_OUTPUT
-          echo "nextRelease=\${nextRelease}" >> $GITHUB_OUTPUT
-          echo "lastRelease=\${lastRelease}" >> $GITHUB_OUTPUT
-        `,
+        verifyReleaseCmd: [
+          'echo "lastVersion=${lastRelease.version}" >> $GITHUB_OUTPUT',
+          'echo "releaseType=${nextRelease.type}" >> $GITHUB_OUTPUT',
+          'echo "nextVersion=${nextRelease.version}" >> $GITHUB_OUTPUT',
+          'echo "nextRelease2=${JSON.encode(nextRelease)}" >> $GITHUB_OUTPUT',
+          'echo "lastRelease2=${JSON.encode(lastRelease)}" >> $GITHUB_OUTPUT',
+          'echo "nextRelease=${nextRelease}" >> $GITHUB_OUTPUT',
+          'echo "lastRelease=${lastRelease}" >> $GITHUB_OUTPUT',
+          'echo "Wrote to GITHUB_OUTPUT"',
+        ].join('\n'),
       },
     ],
     addNpmPlugin(),
