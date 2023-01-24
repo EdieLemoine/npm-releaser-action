215825a4afc2bf235e38004bd233daeb3b531095
Edie Lemoine <edie.lemoine@gmail.com>
2023-01-24 14:22:13 +0100

    test command formats

--- release.config.js
+++ release.config.js
@@ -8,9 +8,6 @@ const plainCommand = `echo "OUTPUTTING TO GITHUB_OUTPUT"
   echo "releaseType=\${nextRelease.type}" >> $GITHUB_OUTPUT
   echo "nextVersion=\${nextRelease.version}" >> $GITHUB_OUTPUT
 
-  echo "nextRelease2=\${JSON.encode(nextRelease)}" >> $GITHUB_OUTPUT
-  echo "lastRelease2=\${JSON.encode(lastRelease)}" >> $GITHUB_OUTPUT
-
   echo "nextRelease=\${nextRelease}" >> $GITHUB_OUTPUT
   echo "lastRelease=\${lastRelease}" >> $GITHUB_OUTPUT
 
